9727bd7449f718ba7daf5cb244fcb2dbc117fc1b
PetkoDeyanov <petkopetkov1001@gmail.com>
2023-12-07 17:52:31 +0200

    Courses View

---
+++ src/main/java/com/example/group4_final_project/controllers/mvc/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.example.group4_final_project.controllers.mvc;
+
+import com.example.group4_final_project.models.DTOs.UserRegisterDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/authentication")
+public class AuthenticationController {
+
+    @ModelAttribute("requestURI")
+    public String requestURI(final HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage(Model model) {
+        model.addAttribute("login", new UserRegisterDto());
+        return "login";
+    }
+}
+
--- src/main/java/com/example/group4_final_project/controllers/mvc/CourseMvcController.java
+++ src/main/java/com/example/group4_final_project/controllers/mvc/CourseMvcController.java
@@ -1,14 +1,24 @@
 package com.example.group4_final_project.controllers.mvc;
 
+import com.example.group4_final_project.services.contracts.CourseService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/courses")
+@RequestMapping()
 public class CourseMvcController {
+
+    private final CourseService courseService;
+
+    @Autowired
+    public CourseMvcController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @ModelAttribute("isAuthenticated")
     public boolean populateIsAuthenticated(HttpSession session) {
@@ -16,12 +26,17 @@ public class CourseMvcController {
     }
 
 
-
-    @GetMapping
-    public String showCoursePage() {
-
+    @GetMapping("/courses")
+    public String showCoursePage(Model model) {
+        var courses = courseService.getAllCourses();
+        model.addAttribute("courses", courses);
 
         return "courses";
+    }
+
+    @GetMapping("/course-details")
+    public String showCourse() {
+        return "course-details";
     }
 
 }
